9a27525f6355438a6fd040cbf952531a9c0459e1
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2024-05-27 09:43:31 +0300

    admin

--- Frontend/MANDAT-FE/src/app/components/cards-list/cards-list.component.ts
+++ Frontend/MANDAT-FE/src/app/components/cards-list/cards-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { ListCardModel } from "src/app/models/cards-list-model";
-
+import { Router } from "@angular/router";
 @Component({
   selector: "app-cards-list",
   templateUrl: "./cards-list.component.html",
@@ -10,11 +10,24 @@ export class CardsListComponent {
   @Input() models: ListCardModel[] = [];
   @Output() deleteEmitter = new EventEmitter<string>();
 
-  constructor() {
+  constructor(private router: Router) {
+    console.log(this.models);
   }
 
   delete(id: string): void {
     this.deleteEmitter.emit(id);
     this.models = this.models.filter(model => model.id !== id);
+  }
+  goToProfile(email: string): void {
+    if (this.isAdminPage()) {
+      this.router.navigate([`user-profile/${email}`]);
+    }
+  }
+  settings(email: string): void {
+    this.router.navigate([`settings/${email}`]);
+  }
+
+  isAdminPage(): boolean {
+    return this.router.url === "/admin-manage-users";
   }
 }
